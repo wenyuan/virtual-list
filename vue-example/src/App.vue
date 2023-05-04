e851b1587574c219a9bdf1ed43a9ab0c8b62a610
wenyuan <xuwenyuan2016@163.com>
2023-05-04 13:13:42 +0800

    feat(example): add vue-example: FixedHeightVirtualList

---
+++ vue-example/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <button @click="loadData">载入数据</button>
+    <CommonList :listData="listData" :itemSize="100" />
+    <!-- <FixedHeightVirtualList :listData="listData" :itemSize="100" /> -->
+  </div>
+</template>
+
+<script>
+import CommonList from './components/CommonList.vue'
+// import FixedHeightVirtualList from './components/FixedHeightVirtualList.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    CommonList,
+    // FixedHeightVirtualList
+  },
+  data() {
+    return {
+      listData: []
+    }
+  },
+  methods: {
+    loadData() {
+      let now = Date.now();
+      let mockData = [];
+      for (let i = 0; i < 100000; i++) {
+        mockData.push({ id: i, value: `第 ${i + 1} 条数据` });
+      }
+      this.listData = mockData
+      console.log('JS运行时间(ms)：', Date.now() - now);
+      setTimeout(() => {
+        console.log('总运行时间(ms)：', Date.now() - now);
+      }, 0)
+    }
+  }
+}
+</script>
+
+<style>
+html {
+  height: 100%;
+}
+
+body {
+  height: 100%;
+  margin: 0;
+}
+
+#app {
+  height: 100%;
+}
+</style>
---
+++ vue-example/src/components/CommonList.vue
@@ -0,0 +1,77 @@
+<template>
+  <div ref="list" class="infinite-list-container">
+    <div class="infinite-list-phantom" :style="{ height: listHeight + 'px' }"></div>
+    <div class="infinite-list">
+      <div ref="items"
+        class="infinite-list-item" 
+        v-for="item in visibleData" 
+        :key="item.id"
+        :style="{ height: itemSize + 'px',lineHeight: itemSize + 'px' }"
+      >{{ item.value }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'VirtualList',
+  props: {
+    //所有列表数据
+    listData:{
+      type:Array,
+      default:()=>[]
+    },
+    //每项高度
+    itemSize: {
+      type: Number,
+      default:200
+    }
+  },
+  computed:{
+    //列表总高度
+    listHeight(){
+      return this.listData.length * this.itemSize;
+    },
+    //获取真实显示列表数据
+    visibleData(){
+      return this.listData;
+    }
+  },
+  mounted() {
+    this.screenHeight = this.$el.clientHeight;
+  }
+};
+</script>
+
+
+<style scoped>
+.infinite-list-container {
+  height: 100%;
+  overflow: auto;
+  position: relative;
+  -webkit-overflow-scrolling: touch;
+}
+
+.infinite-list-phantom {
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  z-index: -1;
+}
+
+.infinite-list {
+  left: 0;
+  right: 0;
+  top: 0;
+  position: absolute;
+  text-align: center;
+}
+
+.infinite-list-item {
+  padding: 10px;
+  color: #555;
+  box-sizing: border-box;
+  border-bottom: 1px solid #999;
+}
+</style>
