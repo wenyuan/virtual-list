e46f3c3db5208581989732fa127e2f1a3167933b
wenyuan <xuwenyuan2016@163.com>
2023-05-12 09:46:26 +0800

    feat: add test case: DynamicHeightVirtualList

--- vue-example/src/App.vue
+++ vue-example/src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div id="app">
     <button @click="loadData">载入数据</button>
+    <button @click="loadDynamicHeightData">载入不定高度的数据</button>
     <!-- <CommonList :listData="listData" :itemSize="100" /> -->
-    <FixedHeightVirtualList :listData="listData" :itemSize="100" />
+    <!-- <FixedHeightVirtualList :listData="listData" :itemSize="100" /> -->
+    <DynamicHeightVirtualList :listData="listData" :estimatedItemSize="100" />
   </div>
 </template>
 
 <script>
+import { faker } from '@faker-js/faker';
 // import CommonList from './components/CommonList.vue'
-import FixedHeightVirtualList from './components/FixedHeightVirtualList.vue'
+// import FixedHeightVirtualList from './components/FixedHeightVirtualList.vue'
+import DynamicHeightVirtualList from './components/DynamicHeightVirtualList.vue';
 
 
 export default {
   name: 'App',
   components: {
     // CommonList,
-    FixedHeightVirtualList
+    // FixedHeightVirtualList,
+    DynamicHeightVirtualList
   },
   data() {
     return {
@@ -28,6 +33,18 @@ export default {
       let mockData = [];
       for (let i = 0; i < 100000; i++) {
         mockData.push({ id: i, value: `第 ${i + 1} 条数据` });
+      }
+      this.listData = mockData
+      console.log('JS运行时间(ms)：', Date.now() - now);
+      setTimeout(() => {
+        console.log('总运行时间(ms)：', Date.now() - now);
+      }, 0)
+    },
+    loadDynamicHeightData() {
+      let now = Date.now();
+      let mockData = [];
+      for (let i = 0; i < 100000; i++) {
+        mockData.push({ id: i, value: faker.lorem.sentences() });  // 随机长文本
       }
       this.listData = mockData
       console.log('JS运行时间(ms)：', Date.now() - now);
